320623a73274ab1d5c2b8ee79e97ee69a85f63ee
Pranav Dabhi <PranavAD36@users.noreply.github.com>
2026-03-22 15:23:11 +0530

    fix: bind server to render port

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,9 @@ const { createServer } = require('./server')
 const { app } = createServer()
 
 const port = Number(process.env.PORT || 4000)
-app.listen(port, () => {
+const host = '0.0.0.0'
+
+app.listen(port, host, () => {
   // eslint-disable-next-line no-console
   console.log(`ScanMark API listening on http://localhost:${port}`)
 })
